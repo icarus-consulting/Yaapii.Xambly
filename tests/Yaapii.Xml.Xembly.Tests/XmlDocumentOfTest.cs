f6f9af6772ee8e507baa5ee8ea9d9b7a7d73c726
Meerownymous <meerow@mirmx.de>
2017-10-26 20:31:36 +0200

    test: add XmlDocumentOfTest

--- tests/Yaapii.Xml.Xembly.Tests/XmlDocumentOfTest.cs
+++ tests/Yaapii.Xml.Xembly.Tests/XmlDocumentOfTest.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using Xunit;
+using System.Xml;
+
 namespace Yaapii.Xml.Xembly.Tests
 {
     public class XmlDocumentOfTest
     {
-        public XmlDocumentOfTest()
+        [Fact]
+        public void GetsDocFromNode()
         {
+            var ns = "fancy-namespace";
+            var doc = new XmlDocument();
+            var node = doc.CreateElement("test");
+           
+            Assert.True(
+                new XmlDocumentOf(node).Value().ChildNodes.Item(0) == node);
         }
     }
 }
