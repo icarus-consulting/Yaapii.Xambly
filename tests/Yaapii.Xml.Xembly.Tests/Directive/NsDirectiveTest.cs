7b1707d33bbe3788c7f14ceffe036aaf89f1a459
Andreas <andreas@KoniglisMacBook.fritz.box>
2017-11-07 21:55:32 +0100

    feat: add NsDirective
    closes #7

---
+++ tests/Yaapii.Xml.Xembly.Tests/Directive/NsDirectiveTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Xml;
+using Xunit;
+using Yaapii.Atoms.List;
+using Yaapii.Xml.Xembly.Arg;
+using Yaapii.Xml.Xembly.Cursor;
+using Yaapii.Xml.Xembly.Directive;
+using Yaapii.Xml.Xembly.Stack;
+
+namespace Yaapii.Xml.Xembly.Tests.Directive
+{
+    public class NsDirectiveTest
+    {
+        [Fact]
+        public void SetsNsAttr() {
+            var dom = new XmlDocument();
+            var root = dom.CreateElement("f");
+            dom.AppendChild(root);
+
+            new NsDirective(
+                new ArgOf("somens")
+            ).Exec(
+                dom,
+                new DomCursor(
+                    new EnumerableOf<XmlNode>(root)
+                ),
+                new DomStack()
+            );
+
+            Assert.True(dom.InnerXml == "<f xmlns=\"somens\" />");
+        }
+    }
+}
