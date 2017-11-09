8f7fc0aebeb1b27140fe27295aeecdca9dd6e1f0
Andreas <andreas@KoniglichesMacBook.local>
2017-11-09 08:03:09 +0100

    feat: add CdataDirective
    closes #5

---
+++ tests/Yaapii.Xml.Xembly.Tests/Directive/CdataDirectiveTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml;
+using Xunit;
+using Yaapii.Atoms.List;
+using Yaapii.Xml.Xembly.Directive;
+
+namespace Yaapii.Xml.Xembly.Tests.Directive
+{
+    public class CdataDirectiveTest
+    {
+        [Fact]
+        public void AddsCdataSectionToCurrentNode() {
+            var dom = new XmlDocument();
+            Assert.True(
+                    new Xembler(
+                            new EnumerableOf<IDirective>(
+                                    new AddDirective("root"),
+                                    new AddDirective("foo"),
+                                    new CdataDirective("Hello World")
+                                )
+                        ).Apply(
+                            dom
+                ).InnerXml == "<root><foo><![CDATA[Hello World]]></foo></root>", "add cdata to current node failed");
+        }
+    }
+}
