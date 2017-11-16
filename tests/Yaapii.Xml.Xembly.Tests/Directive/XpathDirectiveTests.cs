771976ae5fcc5b4e66b7d9d6191f984a4170bf9a
Christian Sauer <postancs@gmx.de>
2017-11-16 16:41:07 +0100

    feat: initial XpathDirectiveTests.cs

---
+++ tests/Yaapii.Xml.Xembly.Tests/Directive/XpathDirectiveTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using Xunit;
+
+namespace Yaapii.Xml.Xembly.Tests.Directive
+{
+    public sealed class XpathDirectiveTests
+    {
+        [Fact]
+        public void Test()
+        {
+            //var dom = new XmlDocument();
+
+            //Assert.True(
+            //    new Xembler(
+            //        new AddDirective("root"),
+            //        new AddDirective("foo"),
+            //        new AddDirective("bar"),
+            //        new UpDirective(),
+            //        new SetDirective("Hello World")
+            //    ).Apply(dom).InnerXml == "<root><foo>Hello World</foo></root>", "Up directive failed");
+        }
+
+    }
+}
