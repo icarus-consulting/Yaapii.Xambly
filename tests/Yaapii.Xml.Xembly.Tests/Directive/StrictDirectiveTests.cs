173449b504513dec8bbe6ad2a9931f6c4fec0be0
CLE <christopher.leppel@icarus-consult.de>
2017-11-16 14:35:05 +0100

    test: adds test for strict directive

---
+++ tests/Yaapii.Xml.Xembly.Tests/Directive/StrictDirectiveTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using Xunit;
+using Yaapii.Atoms.List;
+
+namespace Yaapii.Xml.Xembly.Tests.Directive
+{
+    public sealed class StrictDirectiveTests
+    {
+        [Fact]
+        public void ChecksNumberOfCurrentNodes()
+        {
+            var dom = new XmlDocument();
+
+            Assert.True(
+            new Xembler(
+                    new EnumerableOf<IDirective>(
+                            new AddDirective("root"),
+                            new AddDirective("foo"),
+                            new AddDirective("bar"),
+                            new AddDirective("boom"),
+                            new UpDirective(),
+                            new UpDirective(),
+                            new UpDirective(),
+                            new StrictDirective(4),
+                            new UpDirective(),
+                            new SetDirective("Hello World")
+                        )).Apply(dom).InnerXml == "<root><foo><boom>Hello World</boom></foo></root>", "Up directive failed");
+        }
+    }
+}
