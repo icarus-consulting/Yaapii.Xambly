a45d2ca57410b33d2337f7574a7ded3df0566cc6
CLE <christopher.leppel@icarus-consult.de>
2017-11-16 15:21:40 +0100

    test: extends tests for srict directive
    closes #14

--- tests/Yaapii.Xml.Xembly.Tests/Directive/StrictDirectiveTests.cs
+++ tests/Yaapii.Xml.Xembly.Tests/Directive/StrictDirectiveTests.cs
@@ -21,13 +21,62 @@ namespace Yaapii.Xml.Xembly.Tests.Directive
                             new AddDirective("foo"),
                             new AddDirective("bar"),
                             new AddDirective("boom"),
-                            new UpDirective(),
-                            new UpDirective(),
-                            new UpDirective(),
+                            new XpathDirective("//*"),
                             new StrictDirective(4),
                             new UpDirective(),
                             new SetDirective("Hello World")
-                        )).Apply(dom).InnerXml == "<root><foo><boom>Hello World</boom></foo></root>", "Up directive failed");
+                        )).Apply(dom).InnerXml == "<root><foo><boom>Hello World</boom></foo></root>", "Strict directive failed");
+        }
+
+        [Fact]
+        public void FailsWhenNumberOfCurrentNodesIsTooBig()
+        {
+            var dom = new XmlDocument();
+
+            Assert.Throws(new ImpossibleModificationException("").GetType(), () =>
+             {
+                 new Xembler(
+                         new EnumerableOf<IDirective>(
+                                 new AddDirective("foo"),
+                                 new AddDirective("bar"),
+                                 new UpDirective(),
+                                 new AddDirective("bar"),
+                                 new XpathDirective("/foo/bar"),
+                                 new StrictDirective(1)
+                             )).Apply(dom);
+             });
+        }
+
+        [Fact]
+        public void FailsWhenNumberOfCurrentNodesIsZero()
+        {
+            var dom = new XmlDocument();
+
+            Assert.Throws(new ImpossibleModificationException("").GetType(), () =>
+            {
+                new Xembler(
+                        new EnumerableOf<IDirective>(
+                                new AddDirective("foo"),
+                                new AddDirective("bar"),
+                                new XpathDirective("/foo/bar/boom"),
+                                new StrictDirective(1)
+                            )).Apply(dom);
+            });
+        }
+
+        [Fact]
+        public void FailsWhenNumberOfCurrentNodesIsTooSmall()
+        {
+            var dom = new XmlDocument();
+
+            Assert.Throws(new ImpossibleModificationException("").GetType(), () =>
+            {
+                new Xembler(
+                        new EnumerableOf<IDirective>(
+                                new AddDirective("root"),
+                                new StrictDirective(2)
+                            )).Apply(dom);
+            });
         }
     }
 }
