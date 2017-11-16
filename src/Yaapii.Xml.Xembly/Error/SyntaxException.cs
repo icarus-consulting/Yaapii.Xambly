e57867fe226ab21e7143764641619808d19e0a7f
Christian Sauer <postancs@gmx.de>
2017-11-16 11:39:35 +0100

    feat: add SyntaxException
    closes #15

---
+++ src/Yaapii.Xml.Xembly/Error/SyntaxException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Yaapii.Xml.Xembly
+{
+    public sealed class SyntaxException : Exception
+    {
+        public SyntaxException(string cause, Exception inner) : base (cause, inner)
+        { }
+    }
+}
