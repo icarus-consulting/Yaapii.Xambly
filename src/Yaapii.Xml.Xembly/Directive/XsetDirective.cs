02b33f1a9e301641bd69417d31e6b640d9c31f83
icaOBU <olaf.buerger@icarus-consult.de>
2017-11-16 15:18:46 +0100

    fest: adds the XSetDirective-Class and test

---
+++ src/Yaapii.Xml.Xembly/Directive/XsetDirective.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using Yaapii.Atoms.Text;
+using Yaapii.Xml.Xembly.Arg;
+using System.Xml.XPath;
+using System.Collections;
+using System.Collections.Concurrent;
+
+namespace Yaapii.Xml.Xembly.Directive
+{
+    public sealed class XsetDirective : IDirective
+    {
+        
+        private readonly IArg _expr;
+
+        public XsetDirective(string val)
+        {
+            _expr = new ArgOf(val);
+        }
+
+        public new string ToString()
+        {
+            return new FormattedText("XSET {0}", this._expr).AsString();
+        }
+
+        public ICursor Exec(XmlNode dom, ICursor cursor, IStack stack)
+        { 
+
+            XPathNavigator nav = dom.CreateNavigator();
+            
+
+            Dictionary<XmlNode, string> values = new Dictionary<XmlNode, string>(0);
+
+            foreach (XmlNode node in cursor)
+            {
+                try
+                {
+                    values.Add(node, nav.Evaluate(_expr.Raw()).ToString());
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+
+            foreach (KeyValuePair<XmlNode, string> pair in values)
+            {
+                (pair.Key).InnerText = pair.Value;
+            }
+            return cursor;
+        }
+
+    }
+}
+
+
+//public Directive.Cursor exec(final Node dom,
+//        final Directive.Cursor cursor, final Directive.Stack stack)
+//        throws ImpossibleModificationException
+//{
+//    final XPath xpath = XsetDirective.FACTORY.newXPath();
+
+//    final ConcurrentMap<Node, String> values =
+//            new ConcurrentHashMap<Node, String>(0);
+//        for (final Node node : cursor) 
+//        {
+//            try 
+//            {
+//                values.put(
+//                  node,
+                  //xpath.evaluate(this.expr.raw(), node, XPathConstants.STRING
+                  //              ).toString());
+//            } 
+
+//           catch (final XPathExpressionException ex) 
+//           {
+//                throw new ImpossibleModificationException(String.format("invalid XPath expr '%s'", this.expr), ex);
+//           }
+//        }
+//       
+//        for (final Map.Entry<Node, String> entry : values.entrySet()) 
+//        {
+//            entry.getKey().setTextContent(entry.getValue());
+//        }
+//        return cursor;
+//}
---
+++ tests/Yaapii.Xml.Xembly.Tests/Directive/XsetDirectiveTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.XPath;
+using Xunit;
+using Yaapii.Xml.Xembly.Cursor;
+using Yaapii.Xml.Xembly.Directive;
+using Yaapii.Xml.Xembly.Stack;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Yaapii.Xml.Xembly.Tests.Directive
+{
+    public sealed class XsetDirectiveTests
+    {
+        [Fact]
+        public void setsTextDirectlyIntoDomNodes()
+        {
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                XmlElement root = doc.CreateElement("xxx");
+                XmlElement first = doc.CreateElement("first");
+
+                first.InnerText = "15";
+                root.AppendChild(first);
+                XmlElement second = doc.CreateElement("second");
+                second.InnerText = "13";
+                root.AppendChild(second);
+
+                doc.AppendChild(root);
+                doc.Save("XsetDirectiveTests.xml");
+
+                new XsetDirective("sum(/xxx/*/text()) + 6").Exec(
+                doc, new DomCursor(new List<XmlNode>() { first }),
+                new DomStack());
+                doc.Save("XsetDirectiveTests.xml");
+
+                XPathNavigator nav = doc.CreateNavigator();
+                nav.MoveToFirstChild();
+                nav.MoveToFirstChild();
+
+                Assert.True(nav.Value == "34");
+
+            }
+            finally
+            {
+                if (File.Exists("XsetDirectiveTests.xml"))
+                {
+                    File.Delete("XsetDirectiveTests.xml");
+                }
+            }
+        }
+
+    }
+}
