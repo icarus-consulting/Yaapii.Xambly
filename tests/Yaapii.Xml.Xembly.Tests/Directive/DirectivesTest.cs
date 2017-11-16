3fdfcd4b0e51363c7aab2edde6d6276fd8f63750
Miro Tunk <miro.tunk@icarus-consult.de>
2017-11-16 14:39:03 +0100

    feat: add initial Directives unittest

---
+++ tests/Yaapii.Xml.Xembly.Tests/Directive/DirectivesTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Yaapii.Atoms.List;
+
+namespace Yaapii.Xml.Xembly.Directive.Tests
+{
+    public class DirectivesTest
+    {
+        [Fact]
+        public void ParsesGrammar()
+        {
+            IEnumerable<IDirective> dirs = 
+                new Directives(
+                    "ADD 'yummy directive';"
+            );
+
+            Assert.True(
+                new LengthOf(dirs).Value() == 1);
+
+            //MatcherAssert.assertThat(
+            //    dirs,
+            //    Matchers.< Directive > iterableWithSize(2)
+            //);
+        }
+    }
+}
