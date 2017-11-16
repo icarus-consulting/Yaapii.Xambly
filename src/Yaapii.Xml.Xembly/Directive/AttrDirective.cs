6542be4183da4abe3e4b631a43a3df87e8b0d1bf
Miro Tunk <miro.tunk@icarus-consult.de>
2017-11-16 17:42:23 +0100

    docs: restore comments

--- src/Yaapii.Xml.Xembly/Directive/AttrDirective.cs
+++ src/Yaapii.Xml.Xembly/Directive/AttrDirective.cs
@@ -20,9 +20,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Xml;
 using Yaapii.Atoms;
 using Yaapii.Atoms.Scalar;
@@ -31,26 +28,41 @@ using Yaapii.Xml.Xembly.Arg;
 
 namespace Yaapii.Xml.Xembly
 {
+    /// <summary>
+    /// ATTR directive.
+    /// </summary>
     public sealed class AttrDirective : IDirective
     {
         private readonly IScalar<IArg> _name;
         private readonly IScalar<IArg> _value;
 
-        public AttrDirective(string name, string value) 
+        /// <summary>
+        /// ATTR directive.
+        /// </summary>
+        /// <param name="name">Attribute name</param>
+        /// <param name="value">Text value to set</param>
+        public AttrDirective(string name, string value)
             : this(
                   new ScalarOf<IArg>(() => new ArgOf(name)),
                   new ScalarOf<IArg>(() => new ArgOf(value))
                   )
-        {
+        { }
 
-        }
-
+        /// <summary>
+        /// ATTR directive.
+        /// </summary>
+        /// <param name="name">Attribute name</param>
+        /// <param name="value">Text value to set</param>
         public AttrDirective(IScalar<IArg> name, IScalar<IArg> value)
         {
             _name = name;
             _value = value;
         }
 
+        /// <summary>
+        /// String representation.
+        /// </summary>
+        /// <returns>The string</returns>
         public override string ToString()
         {
             return new FormattedText(
@@ -60,6 +72,13 @@ namespace Yaapii.Xml.Xembly
                         ).AsString();
         }
 
+        /// <summary>
+        /// Execute it in the given document with current position at the given node.
+        /// </summary>
+        /// <param name="dom">Document</param>
+        /// <param name="cursor">Nodes we're currently at</param>
+        /// <param name="stack">Execution stack</param>
+        /// <returns>New current nodes</returns>
         public ICursor Exec(XmlNode dom, ICursor cursor, IStack stack)
         {
             var key = _name.Value().Raw();
