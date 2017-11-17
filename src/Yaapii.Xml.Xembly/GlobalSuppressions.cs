2f5ee0b36a841bacfd4fcb6c2d29690310e5a8b0
Miro Tunk <miro.tunk@icarus-consult.de>
2017-11-17 14:54:17 +0100

    fix: remove unused suppressions

--- src/Yaapii.Xml.Xembly/GlobalSuppressions.cs
+++ src/Yaapii.Xml.Xembly/GlobalSuppressions.cs
@@ -6,11 +6,4 @@
 
 using System;
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Procedural Features", "NoStatics:No Statics", Justification = "<Pending>", Scope = "type", Target = "~T:DopX.App.Tmx.Properties.Resources")]
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Immutability", "Immutability:Fields are readonly or constant", Justification = "<Pending>", Scope = "member", Target = "~F:DopX.App.Tmx.Properties.Resources.resourceMan")]
 [assembly: CLSCompliant(false)]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Procedural Features", "NoProperties:No Properties", Justification = "<Pending>", Scope = "member", Target = "~P:DopX.App.Tmx.Properties.Resources.ResourceManager")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Immutability", "Immutability:Fields are readonly or constant", Justification = "<Pending>", Scope = "member", Target = "~F:DopX.App.Tmx.Properties.Resources.resourceCulture")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Procedural Features", "NoProperties:No Properties", Justification = "<Pending>", Scope = "member", Target = "~P:DopX.App.Tmx.Properties.Resources.icarus")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Procedural Features", "NoProperties:No Properties", Justification = "<Pending>", Scope = "member", Target = "~P:DopX.App.Tmx.Properties.Resources.Culture")]
