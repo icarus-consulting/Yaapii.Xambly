8362705a03a8d09e23a058dbfd862cbee258e043
Christian Sauer <postancs@gmx.de>
2017-11-21 13:48:29 +0100

    test: add test for XpathDirective
    closes #42

--- tests/Yaapii.Xml.Xembly.Tests/Directive/XpathDirectiveTests.cs
+++ tests/Yaapii.Xml.Xembly.Tests/Directive/XpathDirectiveTests.cs
@@ -5,28 +5,22 @@ using System.Text;
 using System.Xml;
 using System.Xml.XPath;
 using Xunit;
+using Yaapii.Atoms.List;
+using Yaapii.Atoms.Scalar;
 using Yaapii.Atoms.Text;
+using Yaapii.Xml.Xembly.Cursor;
+using Yaapii.Xml.Xembly.Stack;
 
 namespace Yaapii.Xml.Xembly.Tests.Directive
 {
     public sealed class XpathDirectiveTests
     {
+        /// <summary>
+        /// XpathDirective can find nodes.
+        /// </summary>
         [Fact]
         public void FindsNodesWithXpathExpression()
         {
-            //var dom = new XmlDocument();
-
-            //var xembler = new Xembler(
-            //    new AddDirective("root"),
-            //    new AddDirective("foo"),
-            //    new SetDirective("Hello World")
-            //).Apply(dom);
-
-            //new Xembler(
-            //    new XpathDirective(@"/root"),
-            //    new XpathDirective(@"/foo"),
-            //    new SetDirective("Changed World")
-            //    ).Apply(dom);
             var dom = new XmlDocument();
 
             new Xembler(
@@ -36,46 +30,98 @@ namespace Yaapii.Xml.Xembly.Tests.Directive
                 new Directives(
                     "XPATH '//*[@bar=1]'; ADD 'test';")).Apply(dom);
 
+            Assert.True(
+                null != FromXPath(
+                    dom.InnerXml.ToString(), "/root/foo[@bar=1]/test") &&
+                null != FromXPath(
+                    dom.InnerXml.ToString(), "/root/bar")
+            );
+        }
 
-            //var dirs = new Directives(
-            //    "ADD 'root'; ADD 'foo'; ATTR 'bar', '1'; UP; ADD 'bar';" 
-            //    + "XPATH '//*[@bar=1]'; ADD 'test';"
-                //new JoinedText(
-                //    "ADD 'root'; ADD 'foo'; ATTR 'bar', '1'; UP; ADD 'bar';",
-                //    "XPATH '//*[@bar=1]'; ADD 'test';"
-                //).AsString()
-            //);
+        /// <summary>
+        /// XpathDirective can ignore empty searches.
+        /// </summary>
+        [Fact]
+        public void IgnoresEmptySearches()
+        {
+            var dom = new XmlDocument();
+            dom.AppendChild(dom.CreateElement("top"));
 
-            //new Xembler(dirs).Apply(dom);
-            //XhtmlMatchers.xhtml(dom),
-            //    XhtmlMatchers.hasXPaths(
-            //        "/root/foo[@bar=1]/test",
-            //        "/root/bar"
+            new Xembler(
+                new Directives(
+                    "XPATH '/nothing'; XPATH '/top'; STRICT '1'; ADD 'hey';")).Apply(dom);
+            Assert.NotNull(FromXPath(dom.InnerXml.ToString(), "/top/hey"));
+        }
 
-            //final Document dom = DocumentBuilderFactory.newInstance()
-            //    .newDocumentBuilder().newDocument();
-            //new Xembler(dirs).apply(dom);
-            //MatcherAssert.assertThat(
-            //    XhtmlMatchers.xhtml(dom),
-            //    XhtmlMatchers.hasXPaths(
-            //        "/root/foo[@bar=1]/test",
-            //        "/root/bar"
-            //    )
-            //);
-
-            Assert.NotNull(
-                FromXPath(
-                    dom.InnerXml.ToString(), "/root/foo[@bar=1]/test")
+        /// <summary>
+        /// XpathDirective can find nodes by XPath.
+        /// </summary>
+        [Fact]
+        public void FindsNodesByXpathDirectly()
+        {
+            var dom = new XmlDocument();
+            var root = dom.CreateElement("xxx");
+            var first = dom.CreateElement("a");
+            root.AppendChild(first);
+            var second = dom.CreateElement("b");
+            root.AppendChild(second);
+            dom.AppendChild(root);
+            Assert.Contains(
+                root,
+                new XpathDirective(
+                    "/*")
+                .Exec(
+                    dom,
+                    new DomCursor(
+                        new EnumerableOf<XmlNode>(
+                            first)),
+                    new DomStack())
                 );
         }
 
         /// <summary>
-        /// A navigator from an Xml and XPath
+        /// XpathDirective can find nodes in empty DOM.
         /// </summary>
-        /// <param name="xml"></param>
-        /// <param name="xpath"></param>
-        /// <returns></returns>
-        private XPathNavigator FromXPath(string xml, string xpath)
+        [Fact]
+        public void FindsNodesInEmptyDom()
+        {
+            var dom = new XmlDocument();
+
+            Assert.Empty(
+                new XpathDirective(
+                    "/some-root").Exec(
+                    dom,
+                    new DomCursor(
+                        new EnumerableOf<XmlNode>()),
+                    new DomStack()));
+        }
+
+        /// <summary>
+        /// XpathDirective can find root in cloned document.
+        /// </summary>
+        [Fact]
+        public void FindsRootInClonedNode()
+        {
+            var dom = new XmlDocument();
+            dom.AppendChild(
+                dom.CreateElement(
+                    "high"));
+            var clone = dom.CloneNode(true);
+            new Xembler(
+                new Directives(
+                    "XPATH '/*'; STRICT '1'; ADD 'boom-5';")).Apply(clone);
+            Assert.NotNull(
+                FromXPath(clone.InnerXml.ToString(), "/high/boom-5"));
+        }
+
+    #region Helper
+    /// <summary>
+    /// A navigator from an Xml and XPath
+    /// </summary>
+    /// <param name="xml"></param>
+    /// <param name="xpath"></param>
+    /// <returns></returns>
+    private XPathNavigator FromXPath(string xml, string xpath)
         {
             var nav =
                 new XPathDocument(
@@ -114,5 +160,6 @@ namespace Yaapii.Xml.Xembly.Tests.Directive
 
             return result;
         }
+        #endregion Helper
     }
 }
