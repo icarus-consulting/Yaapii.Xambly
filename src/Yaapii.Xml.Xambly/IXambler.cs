e74ecc227622565ef7793f1fba2fe983f242bf87
Miro Tunk <miro.tunk@icarus-consult.de>
2017-11-29 15:32:30 +0100

    fix: merge master into branch

---
+++ src/Yaapii.Xml.Xambly/IXambler.cs
@@ -0,0 +1,55 @@
+﻿using System.Xml;
+
+namespace Yaapii.Xml.Xambly
+{
+    /// <summary>
+    /// Interface for Processor of Xembly directives (main entry point to the module)
+    /// </summary>
+    public interface IXambler
+    {
+        /// <summary>
+        /// Should apply all changes to the document/node.
+        /// </summary>
+        /// <returns>Same document/node.</returns>
+        /// <param name="dom">DOM document/node</param>
+        XmlNode Apply(XmlNode dom);
+
+        /// <summary>
+        /// Should apply all changes to the document/node, but redirect all exceptions to a IllegalStateException.
+        /// </summary>
+        /// <returns>The quietly.</returns>
+        /// <param name="dom">DOM.</param>
+        XmlNode ApplyQuietly(XmlNode dom);
+
+        /// <summary>
+        /// Should apply all changes to an empty DOM.
+        /// </summary>
+        /// <returns>The DOM</returns>
+        XmlDocument Dom();
+
+        /// <summary>
+        /// Should apply all changes to an empty DOM, but redirect all exceptions to a IllegalStateException.
+        /// </summary>
+        /// <returns>The quietly.</returns>
+        XmlDocument DomQuietly();
+
+        /// <summary>
+        /// Should escape text before using it as a text value
+        /// </summary>
+        /// <returns>The escaped.</returns>
+        /// <param name="text">Text.</param>
+        string Escaped(string text);
+
+        /// <summary>
+        /// Should convert to XML Document.
+        /// </summary>
+        /// <returns>The xml as string</returns>
+        string Xml();
+
+        /// <summary>
+        /// Should convert to XML Document, but redirect all Exceptions to IllegalStateException.
+        /// </summary>
+        /// <returns>The quietly.</returns>
+        string XmlQuietly();
+    }
+}
---
+++ tests/Yaapii.Xml.Xambly.Tests/XamblerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+using System.Xml;
+
+namespace Yaapii.Xml.Xambly.Tests
+{
+    public class XamblerTest
+    {
+        [Fact]
+        public void MakesChangesToDomDocument()
+        {
+            var doc = new XmlDocument();
+
+            //new Xambler(
+                //new Directives)
+        }
+    }
+}
