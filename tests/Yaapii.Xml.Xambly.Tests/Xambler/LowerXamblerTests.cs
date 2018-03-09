c9ea4435c1852559b8b5ecf3ccc22bd806eba23e
Christian Sauer <postancs@gmx.de>
2018-03-09 11:36:14 +0100

    test: add tests for XML creation with or without header

---
+++ tests/Yaapii.Xml.Xambly.Tests/Xambler/LowerXamblerTests.cs
@@ -0,0 +1,96 @@
+﻿using Xunit;
+
+namespace Yaapii.Xml.Xambly.Tests
+{
+    public sealed class LowerXamblerTests
+    {
+        [Fact(Skip = "Skipped - Fix LowerXambler here")]
+        public void MakesXmlLower()
+        {
+            string xml =
+                new LowerXambler(
+                    new Xambler(
+                        new Directives()
+                            .Add("PagE")
+                        )
+                ).Xml();
+
+            Assert.StartsWith(
+                "<page>",
+                xml
+            );
+        }
+
+        [Fact(Skip = "Skipped until LowerXembler works")]
+        public void CreatesXmlWithHeader()
+        {
+            string xml =
+                new LowerXambler(
+                    new Xambler(
+                        new Directives()
+                            .Add("page")
+                            .Add("child-node").Set(" the text\n")
+                        )
+                ).Xml();
+
+            Assert.StartsWith(
+                "<?xml version=\"1.0\" encoding=\"utf-16\"?>",
+                xml
+            );
+        }
+
+        [Fact(Skip = "Skipped until LowerXembler works")]
+        public void CreatesXmlWithoutHeader()
+        {
+            string xml =
+                new LowerXambler(
+                    new Xambler(
+                        new Directives()
+                            .Add("page")
+                            .Add("child-node").Set(" the text\n")
+                    )
+                ).Xml(false);
+
+            Assert.StartsWith(
+                "<page><child-node>",
+                xml
+            );
+        }
+
+        [Fact(Skip = "Skipped until LowerXembler works")]
+        public void CreatesXmlWithHeaderFromXmlQuietly()
+        {
+            string xml =
+                new LowerXambler(
+                    new Xambler(
+                        new Directives()
+                            .Add("page")
+                            .Add("child-node").Set(" the text\n")
+                    )
+                ).XmlQuietly();
+
+            Assert.StartsWith(
+                "<?xml version=\"1.0\" encoding=\"utf-16\"?>",
+                xml
+            );
+        }
+
+        [Fact(Skip = "Skipped until LowerXembler works")]
+        public void CreatesXmlWithoutHeaderFromXmlQuietly()
+        {
+            string xml =
+                new LowerXambler(
+                    new Xambler(
+                        new Directives()
+                            .Add("page")
+                            .Add("child-node").Set(" the text\n")
+                    )
+                ).XmlQuietly(false);
+
+            Assert.StartsWith(
+                "<page><child-node>",
+                xml
+            );
+        }
+    }
+}
---
+++ tests/Yaapii.Xml.Xambly.Tests/Xambler/XamblerTest.cs
@@ -0,0 +1,71 @@
+﻿using Xunit;
+
+namespace Yaapii.Xml.Xambly.Tests
+{
+    public class XamblerTest
+    {
+        [Fact]
+        public void CreatesXmlWithHeader()
+        {
+            string xml =
+                new Xambler(
+                    new Directives()
+                        .Add("page")
+                        .Add("child-node").Set(" the text\n")
+                ).Xml();
+
+            Assert.StartsWith(
+                "<?xml version=\"1.0\" encoding=\"utf-16\"?>",
+                xml
+            );
+        }
+
+        [Fact]
+        public void CreatesXmlWithoutHeader()
+        {
+            string xml =
+                new Xambler(
+                    new Directives()
+                        .Add("page")
+                        .Add("child-node").Set(" the text\n")
+                ).Xml(false);
+
+            Assert.StartsWith(
+                "<page><child-node>",
+                xml
+            );
+        }
+
+        [Fact]
+        public void CreatesXmlWithHeaderFromXmlQuietly()
+        {
+            string xml =
+                new Xambler(
+                    new Directives()
+                        .Add("page")
+                        .Add("child-node").Set(" the text\n")
+                ).XmlQuietly();
+
+            Assert.StartsWith(
+                "<?xml version=\"1.0\" encoding=\"utf-16\"?>",
+                xml
+            );
+        }
+
+        [Fact]
+        public void CreatesXmlWithoutHeaderFromXmlQuietly()
+        {
+            string xml =
+                new Xambler(
+                    new Directives()
+                        .Add("page")
+                        .Add("child-node").Set(" the text\n")
+                ).XmlQuietly(false);
+
+            Assert.StartsWith(
+                "<page><child-node>",
+                xml
+            );
+        }
+    }
+}
