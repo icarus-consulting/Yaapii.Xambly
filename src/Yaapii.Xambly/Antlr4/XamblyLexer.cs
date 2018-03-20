fda2195dc8f8fbf180bc2588458a8cb927447387
Miro Tunk <miro.tunk@icarus-consult.de>
2018-03-20 14:48:11 +0100

    feat: add CopyOf to Directives and bump nugets

--- src/Yaapii.Xambly/Antlr4/XamblyLexer.cs
+++ src/Yaapii.Xambly/Antlr4/XamblyLexer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// Generated from D:\Daten\GITHUB\Yaapii.Xambly\src\Yaapii.Xml.Xambly\\Antlr4\Xambly.g by ANTLR 4.6
+// Generated from D:\Development\work\Yaapii.Xambly\src\Yaapii.Xambly\\Antlr4\Xambly.g by ANTLR 4.6
 
 // Unreachable code detected
 #pragma warning disable 0162
