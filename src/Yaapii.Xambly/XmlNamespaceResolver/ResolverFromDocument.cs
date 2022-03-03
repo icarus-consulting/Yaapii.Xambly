ba5d405491471467a648328aff08671cdf22567a
Christian Sauer <postancs@gmx.de>
2022-03-03 19:41:01 +0100

    feat: add xml namespace resolver that gets namespaces from xml

---
+++ src/Yaapii.Xambly/XmlNamespaceResolver/ResolverFromDocument.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Linq;
+using Yaapii.Atoms;
+using Yaapii.Atoms.Enumerable;
+using Yaapii.Atoms.Error;
+using Yaapii.Atoms.Func;
+using Yaapii.Atoms.Map;
+using Yaapii.Atoms.Scalar;
+
+namespace Yaapii.Xambly.XmlNamespaceResolver
+{
+    /// <summary>
+    /// A XML namespace resolver that knows all namespaces from a XML document.
+    /// The default namespace (in the root node) can be given a prefix to use it in an XPath.
+    /// Default namespaces defined in child nodes can be prefixed by defining a dictionary.
+    /// </summary>
+    public sealed class ResolverFromDocument : IXmlNamespaceResolver
+    {
+        private readonly IScalar<IXmlNamespaceResolver> resolver;
+
+        /// <summary>
+        /// A XML namespace resolver that knows all namespaces from a XML document.
+        /// The default namespace (in the root node) can be given a prefix to use it in an XPath.
+        /// Default namespaces defined in child nodes can be prefixed by defining a dictionary.
+        /// </summary>
+        public ResolverFromDocument(XNode dom, string rootDefNamespacePrefix = "", params string[] defNamesapceAndPrefixDictionary) : this(
+            dom,
+            rootDefNamespacePrefix,
+            new MapOf(defNamesapceAndPrefixDictionary)
+        )
+        { }
+
+        /// <summary>
+        /// A XML namespace resolver that knows all namespaces from a XML document.
+        /// The default namespace (in the root node) can be given a prefix to use it in an XPath.
+        /// Default namespaces defined in child nodes can be prefixed by defining a dictionary.
+        /// </summary>
+        public ResolverFromDocument(XNode dom, string rootDefNamespacePrefix, IDictionary<string, string> defNamesapceAndPrefixDictionary) : this(
+            ScalarOf.New<IXmlNamespaceResolver>(() =>
+            {
+                var manager =
+                    new XmlNamespaceManager(
+                        new NameTable()
+                    );
+                var root =
+                    XDocument.Parse(
+                        dom.ToString()
+                    ).Root;
+                var rootNsAttributes =
+                    Filtered.New(attr =>
+                        attr.IsNamespaceDeclaration,
+                        root.Attributes()
+                    );
+                Each.New(attr =>
+                    {
+                        var prefix = attr.Name.LocalName;
+                        if (attr.Name.Namespace == XNamespace.None)
+                        {
+                            manager.AddNamespace(
+                                string.Empty,
+                                attr.Value
+                            );
+                            prefix = rootDefNamespacePrefix;
+                        }
+                        manager.AddNamespace(
+                            prefix,
+                            attr.Value
+                        );
+                    },
+                    rootNsAttributes
+                ).Invoke();
+                if (defNamesapceAndPrefixDictionary.Count > 0)
+                {
+                    var childrenDefaultNsAttributes =
+                        Filtered.New(attr =>
+                            attr.IsNamespaceDeclaration,
+                            root.Descendants().Attributes()
+                        );
+                    Each.New(attr =>
+                        {
+                            var prefix = attr.Name.LocalName;
+                            new FailWhen(
+                                attr.Name.Namespace != XNamespace.None,
+                                $"Prefixed namespace declaration ony supported in root node but found in children: {attr}"
+                            ).Go();
+                            if (defNamesapceAndPrefixDictionary.ContainsKey(attr.Value))
+                            {
+                                manager.AddNamespace(
+                                    defNamesapceAndPrefixDictionary[attr.Value],
+                                    attr.Value
+                                );
+                            }
+                            // else: Skipping. The manager knows only one default namespace (from the root node)
+                        },
+                        childrenDefaultNsAttributes
+                    ).Invoke();
+                }
+
+                return manager;
+            })
+        )
+        { }
+
+        private ResolverFromDocument(IScalar<IXmlNamespaceResolver> resolver)
+        {
+            this.resolver = resolver;
+        }
+
+        public IDictionary<string, string> GetNamespacesInScope(XmlNamespaceScope scope)
+        {
+            return
+                this.resolver.Value().GetNamespacesInScope(scope);
+        }
+
+        public string LookupNamespace(string prefix)
+        {
+            return
+                this.resolver.Value().LookupNamespace(prefix);
+        }
+
+        public string LookupPrefix(string namespaceName)
+        {
+            return
+                this.resolver.Value().LookupPrefix(namespaceName);
+        }
+    }
+}
---
+++ tests/Test.Yaapii.Xambly/XmlNamespaceResolver/ResolverFromDocumentTests.cs
@@ -0,0 +1,180 @@
+﻿using System.Xml.Linq;
+using Xunit;
+using Yaapii.Atoms.Map;
+
+namespace Yaapii.Xambly.XmlNamespaceResolver.Test
+{
+    public sealed class ResolverFromDocumentTests
+    {
+        [Fact]
+        public void KnowsDefalutNamespaces()
+        {
+            var expected =
+                new MapOf(
+                    "", "default"
+                );
+            var xml =
+                "<root xmlns=\"default\">" +
+                    "<node />" +
+                "</root>";
+
+            Assert.Equal(
+                expected,
+                new ResolverFromDocument(
+                    XDocument.Parse(xml)
+                ).GetNamespacesInScope(System.Xml.XmlNamespaceScope.ExcludeXml)
+            );
+        }
+
+        [Fact]
+        public void KnowsPrefixedDefalutNamespaces()
+        {
+            var expected =
+                MapOf.New(
+                    "", "default",
+                    "def", "default"
+                );
+            var xml =
+                "<root xmlns=\"default\">" +
+                    "<node />" +
+                "</root>";
+
+            Assert.Equal(
+                expected,
+                new ResolverFromDocument(
+                    XDocument.Parse(xml),
+                    "def"
+                ).GetNamespacesInScope(System.Xml.XmlNamespaceScope.ExcludeXml)
+            );
+        }
+
+        [Fact]
+        public void KnowsDefaultNamespacesFromChildren()
+        {
+            var expected =
+                MapOf.New(
+                    "def1", "childDefault",
+                    "def2", "nodeDefault"
+                );
+            var xml =
+                "<root>" +
+                    "<child xmlns=\"childDefault\">" +
+                        "<node xmlns=\"nodeDefault\" />" +
+                    "</child>" +
+                "</root>";
+
+            Assert.Equal(
+                expected,
+                new ResolverFromDocument(
+                    XDocument.Parse(xml),
+                    "irrelevant",
+                    "childDefault", "def1",
+                    "nodeDefault", "def2"
+                ).GetNamespacesInScope(System.Xml.XmlNamespaceScope.ExcludeXml)
+            );
+        }
+
+
+
+        [Fact]
+        public void KnowsPrefixedNamespace()
+        {
+            var expected =
+                new MapOf(
+                    "pre", "withPrefix"
+                );
+            var xml =
+                "<root xmlns:pre=\"withPrefix\">" +
+                    "<child>" +
+                        "<node />" +
+                    "</child>" +
+                "</root>";
+
+            Assert.Equal(
+                expected,
+                new ResolverFromDocument(
+                    XDocument.Parse(xml)
+                ).GetNamespacesInScope(System.Xml.XmlNamespaceScope.ExcludeXml)
+            );
+        }
+
+        [Fact]
+        public void KnowsDefalutAndPrefixedNamespace()
+        {
+            var expected =
+                MapOf.New(
+                    "", "default",
+                    "pre", "withPrefix",
+                    "x", "anotherWithPrefix"
+                );
+            var xml =
+                "<root xmlns=\"default\" xmlns:pre=\"withPrefix\" xmlns:x=\"anotherWithPrefix\">" +
+                    "<child>" +
+                        "<node />" +
+                    "</child>" +
+                "</root>";
+
+            Assert.Equal(
+                expected,
+                new ResolverFromDocument(
+                    XDocument.Parse(xml)
+                ).GetNamespacesInScope(System.Xml.XmlNamespaceScope.ExcludeXml)
+            );
+        }
+
+        [Fact]
+        public void KnowsPrefixedDefalutAndPrefixedNamespace()
+        {
+            var expected =
+                MapOf.New(
+                    "", "default",
+                    "def", "default",
+                    "pre", "withPrefix"
+                );
+            var xml =
+                "<root xmlns=\"default\" xmlns:pre=\"withPrefix\">" +
+                    "<child>" +
+                        "<node />" +
+                    "</child>" +
+                "</root>";
+
+            Assert.Equal(
+                expected,
+                new ResolverFromDocument(
+                    XDocument.Parse(xml),
+                    "def"
+                ).GetNamespacesInScope(System.Xml.XmlNamespaceScope.ExcludeXml)
+            );
+        }
+
+        [Fact]
+        public void KnowsPrefixedDefaultAndChildrenDefaultAndPrefixedNamespace()
+        {
+            var expected =
+                MapOf.New(
+                    "", "default",
+                    "def", "default",
+                    "pre", "withPrefix",
+                    "x", "anotherWithPrefix",
+                    "def1", "childDefault",
+                    "def2", "nodeDefault"
+                );
+            var xml =
+                "<root xmlns=\"default\" xmlns:pre=\"withPrefix\" xmlns:x=\"anotherWithPrefix\">" +
+                    "<child xmlns=\"childDefault\">" +
+                        "<node xmlns=\"nodeDefault\" />" +
+                    "</child>" +
+                "</root>";
+
+            Assert.Equal(
+                expected,
+                new ResolverFromDocument(
+                    XDocument.Parse(xml),
+                    "def",
+                    "childDefault", "def1",
+                    "nodeDefault", "def2"
+                ).GetNamespacesInScope(System.Xml.XmlNamespaceScope.ExcludeXml)
+            );
+        }
+    }
+}
